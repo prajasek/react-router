26f99ce9f1cb5e0374e4b1d904205bdb526fee71
Prasanth <sekaranp92@gmail.com>
2022-05-22 20:05:19 -0400

    create components

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div> <h1> About</h1> 
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae et facere accusantium, perspiciatis rerum doloremque exercitationem tempora ipsa illum distinctio!
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div>
+        <h1>Blog..</h1>
+        Inventore rem accusamus omnis facilis tempora, optio reiciendis. Inventore temporibus sapiente voluptas quaerat aut corporis consequuntur id a natus iusto. Cumque repellat iure enim. Amet beatae dicta perferendis culpa iusto! Accusantium dignissimos optio quis totam vitae expedita consequatur. Voluptatibus vitae, ratione amet nihil quod quaerat minima similique saepe ipsa unde. Delectus, natus.
+    </div>
+  )
+}
+
+export default Blog
